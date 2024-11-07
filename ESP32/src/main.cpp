9a45f25e2df8593e6df6bef160eafc4cae8ee8a7
Iurii-Podkorytov <i.podkorytov@innopolis.university>
2024-11-07 12:57:38 +0300

    working turn

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -2,40 +2,51 @@
 #include <BluetoothSerial.h>
 #include "esp32-hal-ledc.h"
 
+#define in(a, x, y) (a >= x && a < y) 
+
 #define STOP 0
 #define JOYSTICK 1
-#define LID_OPEN 2
-#define LID_CLOSE 3
-#define AUDIO 4
+#define LID 2
+#define LID_OPEN 1
+#define LID_CLOSE 0
+#define AUDIO 3
 
 BluetoothSerial SerialBT;
 
 // PWM setting
 const int frequency = 30000;   
-const int pwmChannelL = 0;
-const int pwmChannelR = 1;
+const int pwmChannelL1 = 0;
+const int pwmChannelL2 = 1;
+const int pwmChannelR1 = 2;
+const int pwmChannelR2 = 3;
 const int resolution = 8;
 
-#define MOTOR_L_PWM 16 // D0
-#define MOTOR_L_DIR 17 // D1
-#define MOTOR_R_PWM 18 // D2
-#define MOTOR_R_DIR 19 // D3
-// #define MOTOR_LID_PWM 10
-// #define MOTOR_LID_DIR 11
+const int max_speed = pow(2, resolution)-1;
+
+#define MOTOR_L1 25 // D0
+#define MOTOR_L2 26 // D1
+#define MOTOR_R1 27 // D2
+#define MOTOR_R2 33 // D3
+#define MOTOR_LID1 32 // D0
+#define MOTOR_LID2 14 // D1
 
 struct motor {
     uint8_t in1;
     uint8_t in2;
+    uint8_t channel1;
+    uint8_t channel2;
 };
 
-motor motorL = {MOTOR_L_PWM, MOTOR_L_DIR};
-motor motorR = {MOTOR_R_PWM, MOTOR_R_DIR};
-// motor motorLid = {MOTOR_LID_PWM, MOTOR_LID_DIR};
+motor motorL = {MOTOR_L1, MOTOR_L2, pwmChannelL1, pwmChannelL2};
+motor motorR = {MOTOR_R1, MOTOR_R2, pwmChannelR1, pwmChannelR2};
+motor motorLid = {MOTOR_LID1, MOTOR_LID2, 0, 0};
+
+const int min_duty = 80;
 
 void drive(byte, byte);
 void set_speed(motor, int);
-// void open_lid();
-// void close_lid();
+void open_lid();
+void close_lid();
 // void play_audio(byte);
 void stop();
 
@@ -45,51 +56,66 @@ void setup() {
   Serial.println("Bluetooth Started! Ready to pair...");
 
   pinMode(motorL.in1, OUTPUT);
-  pinMode(motorR.in1, OUTPUT);
-  // pinMode(motorLid.dirPin, OUTPUT);
   pinMode(motorL.in2, OUTPUT);
+
+  pinMode(motorR.in1, OUTPUT);
   pinMode(motorR.in2, OUTPUT);
-  // pinMode(motorLid.pwmPin, OUTPUT);
 
-  ledcSetup(pwmChannelL, frequency, resolution);
-  ledcSetup(pwmChannelR, frequency, resolution);
+  pinMode(motorLid.in1, OUTPUT);
+  pinMode(motorLid.in2, OUTPUT);
+
+  ledcSetup(pwmChannelL1, frequency, resolution);
+  ledcSetup(pwmChannelL2, frequency, resolution);
+  ledcSetup(pwmChannelR1, frequency, resolution);
+  ledcSetup(pwmChannelR2, frequency, resolution);
   
-  ledcAttachPin(motorL.in1, pwmChannelL);
-  ledcAttachPin(motorL.in2, pwmChannelL);
+  ledcAttachPin(motorL.in1, pwmChannelL1);
+  ledcAttachPin(motorL.in2, pwmChannelL2);
 
-  ledcAttachPin(motorR.in1, pwmChannelR);
-  ledcAttachPin(motorR.in2, pwmChannelR);
+  ledcAttachPin(motorR.in1, pwmChannelR1);
+  ledcAttachPin(motorR.in2, pwmChannelR2);
 }
 
 
 void loop() {
-  static uint8_t buffer[8] = { 0 };
+  static uint8_t buffer[3] = { 0 };
 
   // Check for incoming serial data
-  if (SerialBT.available() >= 8) {
-    SerialBT.readBytes(buffer, 8);
+  if (SerialBT.available() >= 3) {
+    SerialBT.readBytes(buffer, 3);
+    SerialBT.write(buffer, sizeof(buffer));
+    for (int i = 0; i < 3; i++) {
+      Serial.print(buffer[i]);
+      Serial.print(' ');  
+    }
+    Serial.println();
+
     switch (buffer[0])
     {
     case STOP:
       stop();
+      SerialBT.write('S');
+      Serial.println('S');
       break;
     case JOYSTICK:
-      Serial.print("X ");
-      Serial.print(buffer[1]);
-      Serial.print(" Y ");
-      Serial.println(buffer[2]);
+      SerialBT.write('J');
+      Serial.println('J');
       drive(buffer[1], buffer[2]);
       break;
-    case LID_OPEN:
-      // open_lid();
-      break;
-    case LID_CLOSE:
-      // close_lid();
+    case LID:
+      SerialBT.write('L');
+      Serial.println('L');
+      if (buffer[1] == LID_OPEN)
+          open_lid();
+      else 
+          close_lid();
       break;
     case AUDIO:
       // play_audio(buffer[1]);
       break;
     default:
+      SerialBT.write('D');
+      Serial.println('D');
       stop();
       break;
     }
@@ -102,39 +128,60 @@ void stop() {
 }
 
 void drive(byte X, byte Y) {
-  const int max_speed = pow(2, resolution)-1;
+  int forward = map(Y, 0, 254, -max_speed, max_speed);
+  int turn = -map(X, 0, 254, -max_speed, max_speed);
+  float angle = atan2(forward, turn);
+  if(angle < 0) angle += 2*PI;
+  float radius = sqrt(turn * turn +  forward * forward)/2;
 
-  int forward = map(Y, 0, 250, -max_speed, max_speed);
-  int turn = -map(X, 0, 250, -max_speed, max_speed);
+  Serial.print("A ");
+  Serial.print(angle);
+  Serial.print(" R ");
+  Serial.println(radius);
 
   int dutyR = constrain(turn+forward, -max_speed, max_speed);
   int dutyL = constrain(turn-forward, -max_speed, max_speed);
 
+  float _angle = angle*6;
+  if (in(_angle, 5*PI, 7*PI) && radius > 60) { // right turn
+    dutyR = -max_speed;
+    dutyL = -max_speed;
+    Serial.println("RIGHT");
+  } 
+  else if((in(_angle, 0, PI) || in(_angle, 11*PI, 12*PI)) && radius > 60) { // left turn
+    dutyR = max_speed;
+    dutyL = max_speed;
+    Serial.println("LEFT");
+}
+
   set_speed(motorL, dutyL);
   set_speed(motorR, dutyR);
+}
 
-  Serial.print("L ");
-  Serial.print(dutyL);
-  Serial.print(" R ");
-  Serial.println(dutyR);
+void open_lid() {
+  analogWrite(motorLid.in1, HIGH);
+  analogWrite(motorLid.in2, LOW);
+}
 
+void close_lid() {
+  analogWrite(motorLid.in2, HIGH);
+  analogWrite(motorLid.in1, LOW);
+}
+
+void stop_analog(motor motor) {
+  analogWrite(motorLid.in2, LOW);
+  analogWrite(motorLid.in1, LOW);
 }
 
 void set_speed(motor motor, int speed) {
-  // if (speed >= 0){
-  //   ledcWrite(motor.in1, speed);
-  //   ledcWrite(motor.in2, LOW);
-  // }
-  // else {
-  //   ledcWrite(motor.in2, -speed);
-  //   ledcWrite(motor.in1, LOW);
-  // } 
   if (speed >= 0){
-    analogWrite(motor.in1, speed);
-    analogWrite(motor.in2, LOW);
+    if (speed < min_duty) speed = min_duty;
+    ledcWrite(motor.channel1, speed);
+    ledcWrite(motor.channel2, LOW);
   }
   else {
-    analogWrite(motor.in2, -speed);
-    analogWrite(motor.in1, LOW);
+    if (speed > min_duty) speed = -min_duty;
+    ledcWrite(motor.channel2, -speed);
+    ledcWrite(motor.channel1, LOW);
   } 
 }
